6c1840ff3807c76f2d756dced0015f15bb61b725
Dakota States <dakota.states@gmail.com>
2020-02-23 04:18:44 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,21 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+
+class EmailAddressParser
+  attr_accessor :email_addreses
+  
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+  end
+
+  def parse
+    if @email_addresses.include?"," then
+      @email_addresses.split(",").uniq
+    else
+      @email_addresses.split(" ").uniq
+    end
+  end
+
+end
