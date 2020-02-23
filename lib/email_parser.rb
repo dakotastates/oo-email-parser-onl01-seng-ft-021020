663f7ae6cd9271cb90dade7c14a180a6df77b219
Dakota States <dakota.states@gmail.com>
2020-02-23 04:21:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,7 @@ class EmailAddressParser
 
   def parse
     if @email_addresses.include?"," then
-      @email_addresses.split(",").uniq
+      @email_addresses.split(/[,]/).uniq
     else
       @email_addresses.split(" ").uniq
     end
